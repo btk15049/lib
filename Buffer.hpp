0f8f7a39835a6c0cd64468a429a16dc0a3a9c1dd
ykawabat <ykawabat@yahoo-corp.jp>
2019-05-12 22:26:47 +0900

    update Buffer

--- Buffer.hpp
+++ Buffer.hpp
@@ -12,18 +12,62 @@
  */
 namespace _Buffer_ {
     template <typename T>
-    struct BufferManager {
-        T* mem;
-        int ptr;
-        BufferManager(T* mem) {
-            ptr       = 0;
-            this->mem = mem;
-        }
-        T* get(int m) {
-            ptr += m;
-            return mem + ptr - m;
-        }
-        void reset() { ptr = 0; }
+    class BufferManager {
+      protected:
+        T* buf;
+
+      public:
+        BufferManager(T* buf) : buf(buf) {}
+        virtual T* pop(int size) = 0;
+        virtual void push(T* p)  = 0;
+        virtual void clear()     = 0;
     };
+
+    template <typename T>
+    class DisposableBufferManager : public BufferManager<T> {
+      private:
+        int ptr;
+
+      public:
+        DisposableBufferManager(T* buf) : BufferManager<T>(buf), ptr(0){};
+
+        T* pop(int size) {
+            ptr += size;
+            return this->buf + ptr - size;
+        }
+
+        void push(T* p) {}
+
+        void clear() { ptr = 0; }
+    };
+
+    template <typename T>
+    class BlockBufferManager : public BufferManager<T> {
+      private:
+        std::queue<T*> que;
+
+      public:
+        BlockBufferManager(T* buf, int blocks, int block_size)
+            : BufferManager<T>(buf) {
+            for (int i : range(blocks)) {
+                que.push(this->buf + i * block_size);
+            }
+        };
+
+        T* pop(int size = 0) {
+            T* ptr = que.front();
+            que.pop();
+            return ptr;
+        }
+
+        void push(T* p) { que.push(p); }
+
+        void clear() {}
+    };
+
 } // namespace _Buffer_
 using namespace _Buffer_;
+
+#define BlockBuffer(type, blocks, block_size, var_name) \
+    type var_name##_mem[(blocks) * (block_size)];       \
+    BlockBufferManager<type> var_name(var_name##_mem, (blocks), (block_size));
