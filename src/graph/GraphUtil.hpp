a18a97d8484c9df38e199ad7b3518221d522fba1
btk <btk15049@live.jp>
2019-10-06 22:28:51 +0900

    test commit

--- src/graph/GraphUtil.hpp
+++ src/graph/GraphUtil.hpp
@@ -17,30 +17,30 @@
 /**
  * @brief
  * 通常のダイクストラ
- * @tparam E 重み付き辺構造体　.costをメンバにもつ必要あり
- * @tparam COST_TYPE コストの型
+ * @tparam T 重み付き辺構造体　.costをメンバにもつ必要あり
+ * @tparam U コストの型
  * @param g グラフ
  * @param inf 流量のinf
  * @param s 始点
  * @return std::vector<int>
  */
-template <typename E, typename COST_TYPE>
-std::vector<COST_TYPE> dijkstra(Graph<E>& g, COST_TYPE inf, const int s = 0) {
+template <typename T, typename U>
+std::vector<U> dijkstra(Graph<T>& g, U inf, const int s = 0) {
     //! TODO interval heapを使う
-    std::priority_queue<std::pair<COST_TYPE, int>> que;
-    std::vector<COST_TYPE> d(g.size(), inf);
+    std::priority_queue<std::pair<U, int>> que;
+    std::vector<U> d(g.size(), inf);
 
     // init
     d[s] = 0;
-    que.push(std::make_pair((COST_TYPE)0, s));
+    que.push(std::make_pair((U)0, s));
 
     while (!que.empty()) {
         const int v       = que.top().second;
-        const COST_TYPE c = -que.top().first;
+        const U c = -que.top().first;
         que.pop();
         if (d[v] < c) continue;
         for (const int eid : g[v]) {
-            E& e        = g.i2e(eid);
+            T& e        = g.i2e(eid);
             const int u = e.versus(v);
             if (chmin(d[u], c + e.cost)) {
                 que.push(std::make_pair(-(c + e.cost), u));
