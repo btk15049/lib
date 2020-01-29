e31fe56802c84758553f9765a46e8d9ddc8243fa
btk15049 <btk15049@live.jp>
2020-01-29 14:11:54 +0000

    [ci] update documents

--- docs/html/dir_130f957cc777e1e7397821d3ae1b6d18.js
+++ docs/html/dir_130f957cc777e1e7397821d3ae1b6d18.js
@@ -1,5 +1,6 @@
 var dir_130f957cc777e1e7397821d3ae1b6d18 =
 [
     [ "Matrix.hpp", "d8/d49/_matrix_8hpp.html", "d8/d49/_matrix_8hpp" ],
-    [ "ModInt.hpp", "dd/d33/_mod_int_8hpp.html", "dd/d33/_mod_int_8hpp" ]
+    [ "ModInt.hpp", "dd/d33/_mod_int_8hpp.html", "dd/d33/_mod_int_8hpp" ],
+    [ "Prime.hpp", "d8/db4/_prime_8hpp_source.html", null ]
 ];
