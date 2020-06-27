2d460be59cc1dde72b9f9976f60ad7a57519cfae
btk <btk15049@live.jp>
2020-06-27 22:25:05 +0900

    add doxygen comments

--- src/template/STLExtension.hpp
+++ src/template/STLExtension.hpp
@@ -9,12 +9,26 @@
 /*</head>*/
 
 namespace ext {
+    /**
+     * @brief std::sortのWrapper関数
+     * @tparam Container std::vectorやstd::listを想定
+     * @param container ソートしたいコンテナオブジェクト
+     * @return Container&
+     * ソート後のコンテナオブジェクト==引数に渡したオブジェクト
+     */
     template <typename Container>
     inline Container& sort(Container& container) {
         sort(std::begin(container), std::end(container));
         return container;
     }
 
+    /**
+     * @brief std::reverseのWrapper関数
+     * @tparam Container std::vectorやstd::listを想定
+     * @param container 反転したいコンテナオブジェクト
+     * @return Container&
+     * 反転後のコンテナオブジェクト==引数に渡したオブジェクト
+     */
     template <typename Container>
     inline Container& reverse(Container& container) {
         reverse(std::begin(container), std::end(container));
