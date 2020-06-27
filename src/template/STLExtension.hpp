06a767319240f15848318bcb62abf6e582b0cec3
btk <btk15049@live.jp>
2020-06-27 18:53:42 +0900

    add sort wrapper

---
+++ src/template/STLExtension.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file STLExtension.hpp
+ * @brief STL独自拡張
+ */
+/*<head>*/
+#pragma once
+#include <algorithm>
+#include <iterator>
+/*</head>*/
+
+namespace ext {
+    template <typename Container>
+    Container& sort(Container& container) {
+        sort(std::begin(container), std::end(container));
+        return container;
+    }
+} // namespace ext
---
+++ test/template/STLExtension.test.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file STLExtension.cpp
+ * @author btk
+ * @brief STLExtension.cppのテスト
+ */
+
+//! @cond
+#define BOOST_TEST_MAIN
+//! @endcond
+#include "template/STLExtension.hpp"
+#include <boost/test/included/unit_test.hpp>
+#include <vector>
+
+//! @cond
+BOOST_AUTO_TEST_SUITE(STLExtensionHpp)
+//! @endcond
+
+
+BOOST_AUTO_TEST_CASE(SortTest) {
+    {
+        std::vector<int> args     = {2, 3, 1, 4};
+        std::vector<int> expected = {1, 2, 3, 4};
+        auto& actual              = ext::sort(args);
+        BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(),
+                                      expected.begin(), expected.end());
+        BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(),
+                                      args.begin(), args.end());
+    }
+}
+
+
+//! @cond
+BOOST_AUTO_TEST_SUITE_END();
+//! @endcond
